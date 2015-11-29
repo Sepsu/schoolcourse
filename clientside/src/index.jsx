920b1af3db765cf5ac456221aa403757bb47daa6
Tiki <severi.tikkala@aalto.fi>
2015-11-29 18:42:36 +0100

    Initial commit for clientside application. Added node modules and configurations. A few modules still missing.

---
+++ clientside/src/index.jsx
@@ -0,0 +1,13 @@
+console.log('I am alive!');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const pair = ['Testing', 'React'];
+
+ReactDOM.render(
+   <div>
+  <p>hello world</p>
+  </div>,
+  document.getElementById('app')
+);
