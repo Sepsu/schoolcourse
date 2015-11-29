1fca9352745a00bf2039891936d948b236993088
Tiki <severi.tikkala@aalto.fi>
2015-11-29 20:03:16 +0100

    Added modules for serverside

--- serverside/src/server.js
+++ serverside/src/server.js
@@ -1,5 +1,24 @@
-import Server from 'socket.io';
+
+import http from "http";
+import Express from 'express'
+import sio from 'socket.io';
+
+
+
+
 
 export default function startServer() {
-  const io = new Server().attach(8090);
+
+	
+	const app = new Express();
+	const server = new http.Server(app);
+	server.listen(8001);	
+  	//const io = new sio().listen(server);
+
+  	console.log("Server is listening at port %s",  server.address().port);
+
+  	app.get("/" , function(req, res){
+  		res.send("Hello World!")
+  	})
 }
+
